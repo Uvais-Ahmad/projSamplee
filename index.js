f20321ee60376ae2c284d40e56cc724fbc0e081a
Uvais Ahmad <uvss21@gmail.com>
2022-07-29 20:52:43 +0530

    First Commit create only Express Server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+
+
+app.listen(port , function(err){
+    if(err){console.log(`Error occur while runnig server ${err}`);  return;}
+    console.log('Server is runnig successfully...Of sample Proj');
+
+}); 
