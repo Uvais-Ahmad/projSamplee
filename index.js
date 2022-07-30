065dcdc6bae73c51804492e4b0de6084245906cc
Uvais Ahmad <uvss21@gmail.com>
2022-07-30 17:13:02 +0530

    Adding static files and setup with ExtractStyles IN layouts

--- index.js
+++ index.js
@@ -9,12 +9,18 @@ const ExpressLayout = require('express-ejs-layouts');
 app.set('view engine', 'ejs');
 app.set('views','./views');
 
+//the subpages css is load at top of page now its append on right place
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 //Here we are using that module only Now create "Layout.ejs" file and make a layout
 //This  "layout.ejs" file not to be render. All Files set in Layout file then render auto by the express 
 
 app.use(express.static('assets'));
 app.use(ExpressLayout);
 app.use('/',require('./routers'));
+
+
 
 
 app.listen(port , function(err){
