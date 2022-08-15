f4c6a62d190633fedacf08a680630b8b0d43b726
Uvais Ahmad <uvss21@gmail.com>
2022-08-15 18:23:55 +0530

    CheckAuthentication on Creating post

--- routers/posts.js
+++ routers/posts.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
 const router = express.Router();
+const passport = require('passport');
 
 const postController = require('../controller/posts_controller')
 
-router.post('/create',postController.create);
+//Only auth User can goto this routes
+router.post('/create' , passport.checkAuthentication ,postController.create);
 
 module.exports= router;
