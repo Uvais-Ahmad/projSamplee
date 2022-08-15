f41bcedae01d96101e659f182643480a0913651c
Uvais Ahmad <uvss21@gmail.com>
2022-08-15 20:29:24 +0530

    creating Schema of Comments only

---
+++ models/comment.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    content:{
+        type : String,
+        required : true
+    },
+    //comment belongs to a user
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    post :{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Post'
+    }
+});
+
+//Gives the first Capital Letter of Model Schema
+const Comment = mongoose.model('Comment',commentSchema);
+module.exports = Comment;
--- models/post.js
+++ models/post.js
@@ -6,9 +6,15 @@ const postSchema = new mongoose.Schema({
         type : String,
         required : true
     },
+    //posts belongs to a user i.e. refre to user Schema
     user:{
         type: mongoose.Schema.Types.ObjectId,
         ref:'User'      //Give Schema name which saved in Model()
+    },
+    //include the array of ids of all comments in this post schema itself
+    comment : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Comment'
     }
 },{
     timestamps:true
