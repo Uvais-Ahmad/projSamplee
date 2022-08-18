b7919dce033f1a6663af1c0f018bbe992b13ad79
Uvais Ahmad <uvss21@gmail.com>
2022-08-18 23:40:09 +0530

    Delete Operation add on POST done

--- controller/posts_controller.js
+++ controller/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function( req , res ){
     //this is used to create the data of post 
@@ -10,5 +11,27 @@ module.exports.create = function( req , res ){
         if(err){ console.log('Error in creating a posts'); }
 
         return res.redirect('back');
+    });
+}
+
+//to delete the post 
+module.exports.destroy = function(req , res){
+    //first check is there any post
+    Post.findById( req.params.id , function( err , post ){
+
+        if(err){
+            console.log('Error Occur while finding the post to delete ',err);
+        }
+        //.id means to conver _id into string
+        if( post.user == req.user.id ){
+            post.remove();
+
+            Comment.deleteMany({ post : req.params.id},function(err){
+                if(err){console.log('Occur while deleteing comments of post deleting')}
+                return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
+        }
     });
 }
