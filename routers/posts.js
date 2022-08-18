a3b3bd6b0f66e078544fbc65e02413ff6d935c16
Uvais Ahmad <uvss21@gmail.com>
2022-08-18 23:40:55 +0530

    Delete a post operation is done

--- routers/posts.js
+++ routers/posts.js
@@ -7,4 +7,7 @@ const postController = require('../controller/posts_controller')
 //Only auth User can goto this routes
 router.post('/create' , passport.checkAuthentication ,postController.create);
 
+//used to destroy the post and here we pass the postId using String Params
+router.get('/destroy/:id',passport.checkAuthentication , postController.destroy);
+
 module.exports= router;
