0c9606eceff75ab9513ce4dce7e3a1d690b6e1b9
Uvais Ahmad <uvss21@gmail.com>
2022-08-22 17:01:12 +0530

    Optimized code of Comments cont using Async

--- controller/comments_controller.js
+++ controller/comments_controller.js
@@ -1,50 +1,88 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = function( req , res ){
+module.exports.create =async function( req , res ){
     //first Find is there Any post on which comment post
     //post is the hidden input Value sent by form
-    console.log(req.body);
-    
-    Post.findById(req.body.post , function( err , post ){
-        if(err){console.log(`Error Occur while finding post for comment, ${err}`);}
-
+    try{
+        let post =await Post.findById(req.body.post );
         if(post){
-            Comment.create({
+            let comment =await Comment.create({
                 content : req.body.content,
                 post : req.body.post,
                 user : req.user._id
-            } , function( err , comment){
-                    if(err){ console.log('Error occur while storing comment',err);}
-
-                    post.comments.push(comment);
-                    post.save();
-
-                    res.redirect('/');
             });
-        }  
-    })
+            post.comments.push(comment);
+            post.save();
+            res.redirect('/');
+        }
+    }
+    catch{err}{
+        console.log('Error : ',err);
+    } 
 }
 
-//Used to delete the comment
-module.exports.destroy = function( req , res ){
+// used to delete the particula comment
+module.exports.destroy =async function( req , res ){
     //First Check comment is Exist
-    Comment.findById( req.params.id , function(err , comment ){
-        if(err){ console.log('There is an Error While finding the comment to delete')}
-        
+    try{
+        let comment = await Comment.findById( req.params.id ); 
         if( comment.user == req.user.id ){
-  
             //Comment present on this post
             let postId = comment.post;
             //Now remove the comment
-            comment.remove();
-            //On that post id Find Comment id inside array of comment and Update it
-            Post.findByIdAndUpdate( postId , { $pull : { comments : req.params.id}} , function (err , post){
-                return res.redirect('back');
-            })
+            comment.remove();        
         }
-        else{
-            return res.redirect('back');
-        }
-    });
+    let post = await Post.findByIdAndUpdate( postId , { $pull : { comments : req.params.id}} );
+    return res.redirect('back');
+    }
+    catch(err){
+        console.log(err);
+    }
 }
+
+// module.exports.create = function( req , res ){
+//     //first Find is there Any post on which comment post
+//     //post is the hidden input Value sent by form
+//     console.log(req.body);
+    
+//     Post.findById(req.body.post , function( err , post ){
+//         if(err){console.log(`Error Occur while finding post for comment, ${err}`);}
+
+//         if(post){
+//             Comment.create({
+//                 content : req.body.content,
+//                 post : req.body.post,
+//                 user : req.user._id
+//             } , function( err , comment){
+//                     if(err){ console.log('Error occur while storing comment',err);}
+
+//                     post.comments.push(comment);
+//                     post.save();
+
+//                     res.redirect('/');
+//             });
+//         }  
+//     })
+// }
+
+//Used to delete the comment MUCH NESTING CODE
+// module.exports.destroy = function( req , res ){
+//     //First Check comment is Exist
+//     Comment.findById( req.params.id , function(err , comment ){
+//         if(err){ console.log('There is an Error While finding the comment to delete')}
+//         if( comment.user == req.user.id ){
+//             //Comment present on this post
+//             let postId = comment.post;
+//             //Now remove the comment
+//             comment.remove();
+//             //On that post id Find Comment id inside array of comment and Update it
+//             Post.findByIdAndUpdate( postId , { $pull : { comments : req.params.id}} , function (err , post){
+//                 return res.redirect('back');
+//             })
+//         }
+//         else{
+//             return res.redirect('back');
+//         }
+//     });
+// }
--- controller/home_Controller.js
+++ controller/home_Controller.js
@@ -1,22 +1,10 @@
 const Post = require('../models/post');
 const User = require('../models/users');
+
+//used to show post on homme with post creater name
 module.exports.home = async function(req , res ){
-    //using end() we direct work but we render an ejs file here
-    // res.end('<h1>Controller set and Express start for controller</h1>');
-    // its already stored cookies while requesting the serve console.log(req.cookies);
-
-    //Find all the post
-    // Post.find( {} , function(err , posts){
-    //     if(err){ console.log('Error Occur while find POST'); }
-    //     return res.render('home' , {
-    //         title : 'HomePage',
-    //         posts:posts
-    //     });
-    // });
     //to find all details about refrenced user  Use POPULATE() func
-
     // Post.find({}).populate..popu..exec(callback and find user inside it and then render it )
-
     try{
         let posts = await Post.find({})
         .populate('user')
@@ -24,7 +12,6 @@ module.exports.home = async function(req , res ){
             path:'comments',
             populate:{ path:'user'}
         })
-    
         let users = await User.find({});
     
         return res.render('home' , {
@@ -37,3 +24,26 @@ module.exports.home = async function(req , res ){
         return;
     }
 }
+
+//Without async await it is much nested code
+
+// module.exports.home = function(req , res ){
+//     Post.find({})
+//     .populate('user')
+//     .populate({
+//         path:'comments',
+//         populate:{ path:'user'}
+//     })
+//     .exec(function(err , posts ){
+        
+//         if(err){ console.log('Error Occur while find POST ,',err); }
+        
+//         User.find({} , function( err , users){
+//             return res.render('home' , {
+//                 title : 'HomePage',
+//                 posts:posts ,
+//                 all_users : users
+//             });
+//         })
+//     });
+// }
