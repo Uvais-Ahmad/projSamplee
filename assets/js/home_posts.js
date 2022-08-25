c28efc8ef424c23daa86ada499ae2021d8093f44
Uvais Ahmad <uvss21@gmail.com>
2022-08-25 17:12:50 +0530

    display post using AJAX request

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -9,7 +9,9 @@
                 url: '/posts/create',
                 data: newPostForm.serialize(),
                 success: function(data){
-                   console.log(data);
+                //    console.log(data);
+                    let newPost = newPostDom(data.data.post);
+                    $('.post-container').prepend(newPost);
                 }, error: function(error){
                     console.log(error.responseText);
                 }
@@ -17,5 +19,41 @@
         });
     }
     // method to create a post in DOM
+    let newPostDom = function(post){
+
+        //here we return While Page of '_post.ejs'
+        return(
+            `<div class="post" id="post-${post._id}">
+                <--Thsi si Me -->
+                <small>
+                    <a class="delete-post-button" href="/posts/destroy/${post._id}">X</a>
+                </small>
+            
+            <h3 id="cont">
+                ${post.content}<br>
+            </h3>
+
+            <h4 id="user">
+                ${post.user.name}<br>
+            </h4>
+
+            <div id="comment-container">
+                <h1 id="tag">Comments :</h1>
+                <div class="post-comments-list" id="post-comments-${post._id}">
+                     
+                </div>
+            </div>
+            
+                <form action="/comments/create" method="POST" id="comment-form">
+                    <input type="text" id="content" name="content" placeholder="Add a comment...">
+                    <input type="hidden" name="post" value="${post._id}">
+                    <input type="submit" value="Post" id="comment-btn">
+                </form>
+        </div>`
+        )
+    }
+
+
+
     createPost();
 }
