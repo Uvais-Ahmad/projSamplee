8c2a6e4608b4bf5ddd43b318773bd041f16a6495
Uvais Ahmad <uvss21@gmail.com>
2022-09-07 00:51:04 +0530

    Playing with api retrive and delete data using API

--- controller/api/v1/posts_api.js
+++ controller/api/v1/posts_api.js
@@ -1,6 +1,50 @@
-module.exports.index = function( req , res ){
-    return res.json( 200 , {
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+//this controller used to retrive all posts from DB
+module.exports.index = async function( req , res ){
+
+    //find from DB and render using API
+    let posts = await Post.find({})
+        .sort('-createdAt')     //This is used to sort data via timing
+        .populate('user')
+        .populate({
+            path:'comments',
+            populate:{ path:'user'}
+        })
+
+
+
+    return res.status(200).json({
         message : "List of posts",
-        posts : []
+        posts : posts
     });
+}
+
+//Used for deleting the posts
+module.exports.destroy = async function(req , res){
+    //first check is there any post
+
+    try{
+        let post = await Post.findById( req.params.id )
+
+            //.id means to conver _id into string
+        
+        post.remove();
+        await Comment.deleteMany({ post : req.params.id})
+    
+        return res.status(200).json({
+            message : "Post and associated comments successfully Deleted",  
+
+        })
+        
+    }
+    catch(err){
+        if(err){
+            console.log(`******* ${err} `);
+        }
+        return res.status(500).json({
+            message : 'Internal Server Error'
+        })
+    }
 }
--- routers/api/v1/posts.js
+++ routers/api/v1/posts.js
@@ -4,6 +4,9 @@ const router = express.Router();
 //this is post api controller file
 const postApi = require('../../../controller/api/v1/posts_api');
 
+//retrive posts from DB
 router.get('/',postApi.index)
+// deleing posts from DB
+router.delete('/:id', postApi.destroy);
 
 module.exports = router;
