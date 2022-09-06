d12e1977451ea26e5c6c1c3145e238ec8f8575a8
Uvais Ahmad <uvss21@gmail.com>
2022-09-06 10:25:56 +0530

    setUp and dir Strucuyruyrt

--- routers/index.js
+++ routers/index.js
@@ -17,6 +17,9 @@ router.use('/users' , require('./users'));
 router.use('/posts',require('./posts'));
 router.use('/comments',require('./comments'));
 
+//for the api routes
+router.use('/api',require('./api'));
+
 //for any further routes   , access from here
 // router.use('/routerName', require('routerFile'));
 
