cc1f7b8f6d17d5c259aa2f637d48256d2ff77a10
Uvais Ahmad <uvss21@gmail.com>
2022-10-30 12:04:16 +0530

    JoinChatRoom and notify by the server setup done

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -13,8 +13,22 @@ class chatEngine {
     }
 
     connectionHandler(){
+
+        let self = this;
+
         this.socket.on( 'connect' ,function(){
             console.log('connection established using sockets...!')
+
+            // Here we fired an event using emit(); and send some data alongside ruquest
+            self.socket.emit("join_room",{
+                user_email : self.userEmail,
+                chatroom : 'codeial'
+            })
+
+            //fired event from erver received by the client side
+            self.socket.on('user_joined',(data)=>{
+                console.log("User joined ",data);
+            });
         });
     }
 }
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -8,5 +8,15 @@ module.exports.chatSockets = function(socketServer){
         socket.on('disconnect',()=> {
             console.log("socket disconnected ...!");                                                                                     
         })
+
+        //this data send by emit from client side
+        socket.on('join_room',(data)=>{
+            console.log("Joining request received of ",data.user_email);
+
+            //it will join the existing chatroom if not exist otherwise it create chatroom then join
+            socket.join(data.chatroom);
+            //after Join fired an event to tell everyOne that someOne has joined the chat room .this will receive clientSide
+            io.in(data.chatroom).emit('user_joined',data);
+        })
     })
 }
